f3f20aa1d0f063dc61f75e2a758a98b0fed9281e
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-06-04 16:37:09 +0200

    client : delete predefined CSS / HTML and add outlet, server : changed app/config.js and deleted pre-existing files

--- server/database/tables.js
+++ server/database/tables.js
@@ -1,5 +1,4 @@
 // Import the repository modules responsible for handling data operations on the tables
-const ItemRepository = require("./models/ItemRepository");
 
 // Create an empty object to hold data repositories for different tables
 const tables = {};
@@ -9,7 +8,7 @@ const tables = {};
 /* ************************************************************************* */
 
 // Register each repository as data access point for its table
-tables.item = new ItemRepository();
+
 
 /* ************************************************************************* */
 
