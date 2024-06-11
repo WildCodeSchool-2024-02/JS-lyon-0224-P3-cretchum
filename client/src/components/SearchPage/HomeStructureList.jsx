3dcff7b719bdbf84473b61e810137e13f7f263b3
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-06-11 15:10:39 +0200

    added fetch on main, loaderdata on searchpaged, removed false const peoples and added the right label on homeStructureList

--- client/src/components/SearchPage/HomeStructureList.jsx
+++ client/src/components/SearchPage/HomeStructureList.jsx
@@ -2,26 +2,27 @@ import PropTypes from "prop-types";
 import "./HomeStructureList.css";
 import CatImage from "../../assets/images/cat.png";
 import DogImage from "../../assets/images/dog.png";
+import PersonImage from "../../assets/images/person.jpg";
 
 function HomeStructureList({ structure }) {
   return (
     <div id="userCard">
       <div id="userGeneral">
         <div id="userImg">
-          <img id="userPicture" src={structure.img} alt={structure.name} />
+          <img id="userPicture" src={PersonImage} alt={structure.name} />
         </div>
         <div id="userInfo">
           <h3 id="userName">{structure.name}</h3>
-          <p id="userLocation">{structure.city}</p>
+          <p id="userLocation">{structure.postal_code}</p>
           <p
             className="userStructure"
             id={
-              structure.professionnel === false
+              structure.is_professional === 0
                 ? "userParticulier"
                 : "userChenil"
             }
           >
-            {structure.professionnel === false
+            {structure.is_professional === 0
               ? "particulier"
               : "professionnel"}
           </p>
@@ -30,13 +31,13 @@ function HomeStructureList({ structure }) {
 
       <ul id="userPref">
         <li
-          className={`useranimal ${structure.cat === false ? "animalHidden" : "userCat"}`}
+          className={`useranimal ${structure.cat === 0 ? "animalHidden" : "userCat"}`}
         >
           <img src={CatImage} alt="dessin de chat noir" className="animalImg" />
           <p>Chat</p>
         </li>
         <li
-          className={`useranimal ${structure.dog === false ? "animalHidden" : "userDog"}`}
+          className={`useranimal ${structure.dog === 0 ? "animalHidden" : "userDog"}`}
         >
           <img src={DogImage} alt="dessin de chat noir" className="animalImg" />
           <p>Chien</p>
@@ -54,8 +55,8 @@ export default HomeStructureList;
 HomeStructureList.propTypes = {
   structure: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    city: PropTypes.string.isRequired,
-    professionnel: PropTypes.bool.isRequired,
+    postal_code: PropTypes.number.isRequired,
+    is_professional: PropTypes.bool.isRequired,
     cat: PropTypes.bool.isRequired,
     dog: PropTypes.bool.isRequired,
     price: PropTypes.number.isRequired,
