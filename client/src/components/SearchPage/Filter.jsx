ad3d45bedbff31cb65f2eccb74e0f0233555d137
YohanGH <YohanGH@proton.me>
2024-06-11 18:38:04 +0200

    Create logic filter for user in structure

--- client/src/components/SearchPage/Filter.jsx
+++ client/src/components/SearchPage/Filter.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
 import "./Filter.css";
 import Patoune from "../../assets/logo/1patounes.png";
 
 function Filter() {
+  // Define state for filter criteria
+  const [filters, setFilters] = useState({
+    nom: '',
+    codePostal: '',
+    type: '',
+    structure: '',
+    prix: '',
+  });
+
+  // Handle changes to filter input fields
+  const handleChange = (e) => {
+     // Update the state with the new filter value
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Handle the search action
+  const handleSearch = async () => {
+    // Convert filter object to query string parameters
+    const queryParams = new URLSearchParams(filters).toString();
+    try {
+      // Make the API call with the query parameters
+      const response = await fetch(
+        `http://localhost:3310/api/homestructure?${queryParams}`
+      );
+      // Check if the response is not okay, throw an error
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      // Parse the response data as JSON
+      const data = await response.json();
+      // Return the data (you may handle it differently in your app)
+      return data;
+    } catch (err) {
+      // Log the error to the console
+      console.error('Fetch error:', err);
+
+      // Return null in case of an error
+      return null;
+    }
+  };
+
   return (
     <div id="filterDiv">
       <div id="imgSearch">
@@ -9,24 +54,30 @@ function Filter() {
         <input
           type="text"
           className="filterInput"
-          id="seachInput"
+          id="searchInput"
+          name="nom"
+          value={filters.nom}
+          onChange={handleChange}
           maxLength={255}
-          placeholder="Rehercher par nom"
+          placeholder="Rechercher par nom"
         />
       </div>
       <div id="filterConditions">
         <ul id="filterList">
           <li className="filter">
             <input
-              type="text"
-              className="filterInput "
-              placeholder="Lieu"
+              type="number"
+              className="filterInput"
+              name="codePostal"
+              value={filters.codePostal}
+              onChange={handleChange}
+              placeholder="Code postal"
               maxLength={55}
             />
           </li>
           <li className="filter">
-            <select className="filterInput">
-              <option value="" disabled selected>
+            <select className="filterInput" name="type" value={filters.type} onChange={handleChange}>
+              <option value="" disabled>
                 Animal
               </option>
               <option value="tous">Tous</option>
@@ -34,10 +85,9 @@ function Filter() {
               <option value="chien">Chien</option>
             </select>
           </li>
-
           <li className="filter">
-            <select className="filterInput">
-              <option value="" disabled selected>
+            <select className="filterInput" name="structure" value={filters.structure} onChange={handleChange}>
+              <option value="" disabled>
                 Type de structure
               </option>
               <option value="tous">Tous</option>
@@ -45,10 +95,9 @@ function Filter() {
               <option value="professionnel">Professionnel</option>
             </select>
           </li>
-
           <li className="filter">
-            <select className="filterInput">
-              <option value="" disabled selected>
+            <select className="filterInput" name="prix" value={filters.prix} onChange={handleChange}>
+              <option value="" disabled>
                 Prix
               </option>
               <option value="tous">Tous les prix</option>
@@ -58,6 +107,7 @@ function Filter() {
           </li>
         </ul>
       </div>
+      <button type="button" onClick={handleSearch} className="buttonRechercher">Rechercher</button>
     </div>
   );
 }
