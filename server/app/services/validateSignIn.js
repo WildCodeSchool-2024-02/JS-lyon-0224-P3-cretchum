bc665c97f47fd5f525350df7006d4933ed22bf8d
JorisB <jorisbudin@gmail.com>
2024-06-12 12:31:43 +0200

    change pattern for lastname and firstname to allow accent and add a user in UserSeeder

--- server/app/services/validateSignIn.js
+++ server/app/services/validateSignIn.js
@@ -2,12 +2,12 @@ const Joi = require("joi");
 
 const signInSchema = Joi.object({
   lastname: Joi.string()
-    .pattern(/^[a-zA-Z\s]+$/, { name: "alpha" }) // Autorise les lettres majuscules, minuscules et les espaces
+    .pattern(/^[a-zA-ZÀ-ÿ\s']+$/)
     .min(2)
     .max(55)
     .required(),
   firstname: Joi.string()
-    .pattern(/^[a-zA-Z\s]+$/, { name: "alpha" }) // Autorise les lettres majuscules, minuscules et les espaces
+    .pattern(/^[a-zA-ZÀ-ÿ\s']+$/)
     .min(2)
     .max(55)
     .required(),
@@ -24,14 +24,11 @@ const signInSchema = Joi.object({
 });
 
 const validateSignIn = (req, res, next) => {
-  // console.log("Request body:", req.body); // Log de la requête reçue
   const { error } = signInSchema.validate(req.body, { abortEarly: false });
 
   if (!error) {
-    // console.log("Validation passed");
     next();
   } else {
-    // console.log("Validation error:", error.details);
     res.status(400).json({ validationErrors: error.details });
   }
 };
