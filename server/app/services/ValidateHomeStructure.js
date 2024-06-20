4001d6843ed67869b321d4e349189566155c2927
JorisB <jorisbudin@gmail.com>
2024-06-20 16:42:57 +0200

    fix middleware_home_structrure

--- server/app/services/ValidateHomeStructure.js
+++ server/app/services/ValidateHomeStructure.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
 const homeStructureSchema = Joi.object({
+  isProfessional: Joi.number().required(),
   name: Joi.string().min(3).max(55).required(),
   lastname: Joi.string()
     .pattern(/^[a-zA-ZÀ-ÿ\s']+$/)
@@ -13,15 +14,14 @@ const homeStructureSchema = Joi.object({
     .max(55)
     .required(),
   phoneNumber: Joi.string().pattern(/[0-9]/).min(10).max(10).required(),
-  location: Joi.string().min(3).max(255).required(),
   postalCode: Joi.string().min(5).max(5).required(),
+  location: Joi.string().min(3).max(255).required(),
   mail: Joi.string().email().required(),
-  password: Joi.string().min(12).required(),
   capacity: Joi.number().min(1).required(),
-  isProfessional: Joi.boolean().required(),
-  cat: Joi.boolean().required(),
-  dog: Joi.boolean().required(),
   price: Joi.number().required(),
+  cat: Joi.number().required(),
+  dog: Joi.number().required(),
+  password: Joi.string().min(12).required(),
   description: Joi.string().min(0).max(800),
 });
 
