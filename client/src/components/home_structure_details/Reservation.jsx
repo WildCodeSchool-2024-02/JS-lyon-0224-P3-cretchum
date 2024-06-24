fe5956e31671b5d03ac5e6b940afdf2759ab9387
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-06-24 11:57:52 +0200

    removed allemand language in import

--- client/src/components/home_structure_details/Reservation.jsx
+++ client/src/components/home_structure_details/Reservation.jsx
@@ -5,7 +5,6 @@ import "./Reservation.css";
 
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import "dayjs/locale/de";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
 import "dayjs/locale/fr";
