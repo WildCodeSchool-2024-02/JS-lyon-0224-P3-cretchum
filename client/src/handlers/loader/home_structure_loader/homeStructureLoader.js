b96439c102a54a60638283b49d1da4ede1aea846
YohanGH <YohanGH@proton.me>
2024-06-25 17:24:12 +0200

    move folder handler in src

--- client/src/handlers/loader/home_structure_loader/homeStructureLoader.js
+++ client/src/handlers/loader/home_structure_loader/homeStructureLoader.js
@@ -1,9 +1,9 @@
 const URL = import.meta.env.VITE_API_URL;
 
 const homeStructureLoader = async ({ params }) => {
-  const response = await fetch(`${URL}/homestructure/${params.id}`);
+  const response = await fetch(`${URL}/reservation/${params.id}`);
   const data = await response.json();
-  if (!response.ok) {
+  if (response.ok !== 200) {
     throw new Error("erreur lors de la récupération des données");
   }
   return data;
