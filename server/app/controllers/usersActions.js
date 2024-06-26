6abebc71b4d70b39a0056552bde39c3ce5cad9be
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-06-26 12:15:55 +0200

    added sameSit attribut

--- server/app/controllers/usersActions.js
+++ server/app/controllers/usersActions.js
@@ -1,6 +1,6 @@
 // Import access to database tables
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const tables = require("../../database/tables");
 
 // The B of BREAD - Browse (Read All) operation
@@ -22,7 +22,7 @@ const read = async (req, res, next) => {
   try {
     // Fetch a specific user from the database based on the provided ID
     const user = await tables.user.read(req.params.id);
-    
+
     // If the user is not found, respond with HTTP 404 (Not Found)
     // Otherwise, respond with the user in JSON format
     if (user == null) {
@@ -75,7 +75,6 @@ const add = async (req, res, next) => {
 
     // Respond with HTTP 201 (Created) and the ID of the newly inserted user
     res.status(201).json({ insertId });
-
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -105,7 +104,11 @@ const checkLog = async (req, res, next) => {
     const user = await tables.user.login(mail);
 
     // Check that the user exists and that the password is correct
-    if (user !== null && user !== undefined && await bcrypt.compare(password, user.password)) {
+    if (
+      user !== null &&
+      user !== undefined &&
+      (await bcrypt.compare(password, user.password))
+    ) {
       // Check if the user has any animals
       let hasAnimals = true;
       if (user.user_id === null) {
@@ -116,26 +119,26 @@ const checkLog = async (req, res, next) => {
       const token = jwt.sign(
         { sub: user.id, hasAnimals },
         process.env.APP_SECRET,
-        { expiresIn: '1d' }
+        { expiresIn: "1d" }
       );
 
       // Remove password from request body
       delete req.body.password;
 
       // Set the token in cookie
-      res.cookie("cookie", token, { httpOnly: true }, { maxAge: 24 * 60 * 60 * 1000 });
+      res.cookie("cookie", token, {
+        httpOnly: true,
+        sameSite: "Strict",
+        maxAge: 24 * 60 * 60 * 1000,
+      });
       res.status(200).json();
-
     } else {
-      res.status(401).json({ error: 'accès non autorisé' });
+      res.status(401).json({ error: "accès non autorisé" });
     }
   } catch (err) {
     next(err);
   }
 };
-
-
-
 
 // Ready to export the controller functions
 module.exports = {
