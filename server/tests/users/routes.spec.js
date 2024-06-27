b7295d08150542278dbe492fe7ef63c507e27b2f
YohanGH <YohanGH@proton.me>
2024-06-27 11:36:20 +0200

    modification test

--- server/tests/users/routes.spec.js
+++ server/tests/users/routes.spec.js
@@ -1,5 +1,5 @@
 // Import required dependencies
-
+const jwt = require('jsonwebtoken');
 const { app, request, database } = require("../config");
 
 // TEST : suite for the GET /api/users route
@@ -24,37 +24,46 @@ describe("GET /api/users", () => {
 // TEST : suite for the GET /api/users/:id route
 
 describe("GET /api/users/:id", () => {
+  const userId = 1;
+  const validToken = jwt.sign({ id: userId }, process.env.APP_SECRET);
+  
   it("should fetch a single user successfully", async () => {
     // Mock rows returned from the database
-    const rows = [{}];
+    const rows = [{ id: userId, name: 'John Doe' }];
 
     // Mock the implementation of the database query method
-    jest.spyOn(database, "query").mockImplementation(() => [rows]);
+    jest.spyOn(database, "query").mockResolvedValue([rows]);
 
     // Send a GET request to the /api/users/:id endpoint
-    const response = await request(app).get(`/api/users/1`);
+    const response = await request(app)
+      .get(`/api/users/${userId}`)
+      .set('Cookie', `cookie=${validToken}`);
 
     // Assertions
     expect(response.status).toBe(200);
     expect(response.body).toStrictEqual(rows[0]);
   });
+
   it("should return 404 for non-existent user", async () => {
     // Mock empty rows returned from the database
     const rows = [];
 
     // Mock the implementation of the database query method
-    jest.spyOn(database, "query").mockImplementation(() => [rows]);
+    jest.spyOn(database, "query").mockResolvedValue([rows]);
 
     // Send a GET request to the /api/users/:id endpoint with an invalid ID
-    const response = await request(app).get("/api/users/0");
+    const response = await request(app)
+      .get("/api/users/0")
+      .set('Cookie', `cookie=${validToken}`);
 
     // Assertions
     expect(response.status).toBe(404);
-    expect(response.body).toEqual({});
+    expect(response.body).toEqual({ error: "User not found" });
   });
 });
 
 // TEST : suite for PUT Route to edit an existing user
+
 
 
 // TEST : suite for the POST /api/users route
