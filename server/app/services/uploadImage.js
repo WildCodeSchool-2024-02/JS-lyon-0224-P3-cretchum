9b4291b154b0b383b6e3e128d4a0d34a1ac5e102
JorisB <jorisbudin@gmail.com>
2024-07-05 15:52:17 +0200

    explicite some condition

--- server/app/services/uploadImage.js
+++ server/app/services/uploadImage.js
@@ -4,13 +4,13 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/assets/avatars/");
+    cb(null, "public/assets/avatars/"); // Path of the folder where file is storage.
   },
   filename: (req, file, cb) => {
     const userId = req.params.id;
     cb(
       null,
-      // Give user ID name to the image and keep the original extension.
+      // Give user ID to the image name and keep the original extension.
       `${userId}${path.extname(file.originalname)}`
     );
   },
@@ -20,11 +20,11 @@ function checkFileType(file, cb) {
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
+  if (mimetype === true && extname === true) {
     return cb(null, true);
   }
 
-  return cb("Error: Images seulement !");
+  return cb("Error: Images seulement");
 }
 const upload = multer({
   storage,
