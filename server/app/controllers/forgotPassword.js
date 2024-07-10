2ee410178b2c5d6ebab962e8ca5a58ad94039faf
YohanGH <YohanGH@proton.me>
2024-07-10 14:58:26 +0200

    remove gmail add mailtrap
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- server/app/controllers/forgotPassword.js
+++ server/app/controllers/forgotPassword.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 const tables = require("../../database/tables");
 
-const { GMAIL, GMAIL_USER_PASSWORD } = process.env;
+const { MAILTRAP_PASSWORD, MAILTRAP_USER } = process.env;
 
 const forgotPassword = async (req, res) => {
   try {
@@ -13,39 +13,43 @@ const forgotPassword = async (req, res) => {
       return res.status(400).send("User with this email does not exist.");
     }
 
+    // Ajout d'un token temporaire pour la modification du mots de passe ajout d'une colone dans la table user resetpasswordtoken avec une date d'expiration. faire la logic avec la gestion du temps puis dans la le resetPasswords faire une correspondances de token.
+
     const token = crypto.randomBytes(20).toString("hex"); // Generate a random token
     user.resetPasswordToken = token; // Assign the token to the user
     user.resetPasswordExpires = Date.now() + 3600000; // Set expiration time for the token (1 hour)
-    await user.save(); // Save the user with the new token and expiration time
 
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: GMAIL,
-        pass: GMAIL_USER_PASSWORD,
-      },
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+       auth: {
+         user: MAILTRAP_USER,
+         pass: MAILTRAP_PASSWORD,
+       },
     });
 
     const mailOptions = {
-      to: user.email,
-      from: GMAIL,
+      from: '"Test Email" <test@email.com>',
+      to: 'regnier.jeux@hotmail.com',
       subject: "Réinitialisation du mot de passe",
       text: `Vous recevez cet email car vous (ou quelqu'un d'autre) avez demandé la réinitialisation du mot de passe de votre compte.\n\n
         Veuillez cliquer sur le lien suivant, ou le copier dans votre navigateur pour compléter le processus :\n\n
         http://${req.headers.host}/reset/${token}\n\n
         Si vous n'avez pas demandé cela, veuillez ignorer cet email et votre mot de passe restera inchangé.\n`,
     };
-
+    
     const info = await transporter.sendMail(mailOptions);
-    if (info !== undefined) {
-      console.error("There was an error sending the email");
-      return res.status(500).send("Error sending email");
+    if (info) {
+      return res.status(250).json("Recovery email sent");
     }
-    return res.status(200).json("Recovery email sent");
+    console.error("There was an error sending the email");
+    return res.status(500).send("Error sending email");
   } catch (error) {
     console.error("Error in forgotPassword:", error);
     return res.status(500).send("An error occurred");
   }
 };
+
+
 
 module.exports = forgotPassword;
