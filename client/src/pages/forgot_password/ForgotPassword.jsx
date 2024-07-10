7a793ca2315bb9de06fc3a977a4651c53e664434
YohanGH <YohanGH@proton.me>
2024-07-10 16:36:38 +0200

    add components navmenu
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- client/src/pages/forgot_password/ForgotPassword.jsx
+++ client/src/pages/forgot_password/ForgotPassword.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import notify from "../../utils/notify";
+import "./ForgotPassword.css";
+
+import NavMenu from "../../components/nav_menu/NavMenu";
 
 function ForgotPassword() {
   const URL = import.meta.env.VITE_API_URL;
@@ -19,11 +22,17 @@ function ForgotPassword() {
       });
 
       if (response.status === 250) {
-        notify("Un e-mail de réinitialisation du mot de passe a été envoyé.", "success");
+        notify(
+          "Un e-mail de réinitialisation du mot de passe a été envoyé.",
+          "success"
+        );
         navigate(`/`);
       } else {
         const errorData = await response.json();
-        notify(errorData.message || "Une erreur est survenue. Veuillez réessayer.", "error");
+        notify(
+          errorData.message || "Une erreur est survenue. Veuillez réessayer.",
+          "error"
+        );
       }
     } catch (err) {
       notify("Une erreur est survenue. Veuillez réessayer.", "error");
@@ -32,19 +41,24 @@ function ForgotPassword() {
 
   return (
     <div>
+      <NavMenu />
       <h2>Réinitialisation du mot de passe</h2>
-      <form onSubmit={handleSubmit}>
+      <form method="post" id="forgotPassword" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Adresse e-mail :</label>
           <input
+            className="forgotPasswordInput"
             type="email"
+            name="mail"
             id="email"
+            minLength={3}
+            maxLength={254}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <button type="submit">Envoyer</button>
+        <button type="submit" id="forgotPasswordButton" className="buttonType1">Envoyer</button>
       </form>
     </div>
   );
