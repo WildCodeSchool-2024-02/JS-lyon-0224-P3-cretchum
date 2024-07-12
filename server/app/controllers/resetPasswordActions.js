7e790f337248f71eb8d15895b25e0454086a28e8
YohanGH <YohanGH@proton.me>
2024-07-12 11:49:27 +0200

    modification all logic for resetPasswordAction add hash password
    Signed-off-by: YohanGH <YohanGH@proton.me>

--- server/app/controllers/resetPasswordActions.js
+++ server/app/controllers/resetPasswordActions.js
@@ -2,24 +2,37 @@ const bcrypt = require("bcryptjs");
 const tables = require("../../database/tables");
 
 const resetPassword = async (req, res) => {
-  const user = await tables.user.read({
-    resetPasswordToken: req.params.token,
-    resetPasswordExpires: { $gt: Date.now() },
-  });
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
 
-  if (user !== undefined) {
-    return res
-      .status(400)
-      .send("Password reset token is invalid or has expired.");
+    // Read user with valid, unexpired reset token
+    const user = await tables.user.readByResetToken(token);
+
+    if (user !== null && user !== undefined || user.resetPasswordExpires < Date.now()) {
+      return res
+        .status(400)
+        .send("Password reset token is invalid or has expired.");
+    }
+
+    // Hachage password
+    // Set the number of rounds to generate the salt used in the hash
+    const saltRounds = 10;
+
+    // Hide user password with bcrypt and number of saltRounds
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Update password and delete token and expiry date
+    await tables.user.updatePasswordAndClearResetToken(user.id, hashedPassword);
+
+    // Delete password in clear
+    delete req.body.password;
+
+    return res.status(200).send("Password has been reset.");
+  } catch (error) {
+    console.error("Error in resetPassword:", error);
+    return res.status(500).send("An error occurred");
   }
-
-  const { password } = req.body;
-  user.password = bcrypt.hashSync(password, 8);
-  user.resetPasswordToken = undefined;
-  user.resetPasswordExpires = undefined;
-  await user.save();
-
-  return res.status(200).send("Password has been reset.");
 };
 
 module.exports = resetPassword;
